bd45453dcdf47f72b06dbe2dea11dc84f3e745ed
F-KZ <supreme-soul@hotmail.fr>
2025-04-08 14:29:37 +0200

    vercel 7

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
   },
   base: '/',
 })
